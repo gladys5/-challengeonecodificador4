a01148794492aed470efa35d19f48866a1791702
gladys5 <yumanji@live.com>
2022-12-22 07:59:38 -0700

    vercion 1440px

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const img = document.querySelector(".img")
+const buttonBlue = document.querySelector(".blue")
+const active = document.querySelector(".active")
+const text = document.querySelector(".text")
+const phragraph = document.querySelector(".phragraph")
+const msn = document.querySelector(".msn")
+const btn = document.querySelector(".btn")
+const btnActive = document.querySelector(".btnActive")
+const textareaView = document.querySelector(".textareaView")
+buttonBlue.onclick = () => {
+  img.classList.toggle("active")
+  phragraph.classList.toggle("active")
+  msn.classList.toggle("active")
+  btn.classList.toggle("btnActive")
+  textareaView.classList.toggle("btnActive")
+  console.log("blue")
+}
+function show(value) {
+  document.getElementById("view").innerHTML = value
+}
 function encrypter(letterEncripter) {
   let phrase = ""
   let phrase2 = ""
