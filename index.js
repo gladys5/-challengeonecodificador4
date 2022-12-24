b3d283a23486923a535a41e967e05bfe6f4f31e1
gladys5 <yumanji@live.com>
2022-12-23 21:45:08 -0700

    refresh

--- index.js
+++ index.js
@@ -6,24 +6,25 @@ const phragraph = document.querySelector(".phragraph")
 const msn = document.querySelector(".msn")
 const btn = document.querySelector(".btn")
 const btnActive = document.querySelector(".btnActive")
-const textareaView = document.querySelector(".textareaView")
+let textareaView = document.querySelector(".textareaView")
 buttonBlue.onclick = () => {
   img.classList.toggle("active")
   phragraph.classList.toggle("active")
   msn.classList.toggle("active")
   btn.classList.toggle("btnActive")
   textareaView.classList.toggle("btnActive")
+  textareaView = encrypter()
   console.log("blue")
 }
 function show(value) {
   document.getElementById("view").innerHTML = value
 }
-function encrypter(letterEncripter) {
+function encrypter(text) {
   let phrase = ""
   let phrase2 = ""
 
-  for (let i = 0; i < letterEncripter.length; i++) {
-    phrase += letterEncripter[i].toLowerCase()
+  for (let i = 0; i < text; i++) {
+    phrase += text[i].toLowerCase()
     if (phrase.includes("a")) {
       phrase += "enter"
     }
@@ -46,4 +47,4 @@ function encrypter(letterEncripter) {
   console.log(phrase2)
   return phrase2
 }
-encrypter("Hola")
+encrypter(show(text.value))
