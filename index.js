807dfb30ab39dbc5e5291c354ec4f8ef80fb0cff
gladys5 <yumanji@live.com>
2022-12-25 23:34:21 -0700

    encrypter cambios

--- index.js
+++ index.js
@@ -1,50 +1,70 @@
 const img = document.querySelector(".img")
 const buttonBlue = document.querySelector(".blue")
 const active = document.querySelector(".active")
-const text = document.querySelector(".text")
 const phragraph = document.querySelector(".phragraph")
 const msn = document.querySelector(".msn")
 const btn = document.querySelector(".btn")
 const btnActive = document.querySelector(".btnActive")
 let textareaView = document.querySelector(".textareaView")
+let btnWhite = document.querySelector(".white")
+
 buttonBlue.onclick = () => {
   img.classList.toggle("active")
   phragraph.classList.toggle("active")
   msn.classList.toggle("active")
   btn.classList.toggle("btnActive")
   textareaView.classList.toggle("btnActive")
-  textareaView = encrypter()
-  console.log("blue")
+  show(encrypter())
 }
-function show(value) {
-  document.getElementById("view").innerHTML = value
+function show(text) {
+  document.getElementById("view").innerHTML = text
 }
-function encrypter(text) {
+
+function encrypter() {
+  let text = document.querySelector(".text").value.toLowerCase()
+
   let phrase = ""
   let phrase2 = ""
 
-  for (let i = 0; i < text; i++) {
-    phrase += text[i].toLowerCase()
-    if (phrase.includes("a")) {
-      phrase += "enter"
-    }
-    if (phrase.includes("e")) {
-      phrase += "imes"
-    }
-    if (phrase.includes("i")) {
-      phrase += "ai"
-    }
-    if (phrase.includes("o")) {
-      phrase += "ober"
-    }
-    if (phrase.includes("u")) {
-      phrase += "ufat"
-    }
+  console.log(text)
 
-    // console.log(phrase)
+  phrase = text
+    .toLowerCase()
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("o", "ober")
+    .replaceAll("a", "ai")
+    .replaceAll("u", "ufat")
+
+  if (phrase == "") {
+    alert("por favor ingrese una frase")
   }
+
   phrase2 = phrase
-  console.log(phrase2)
+
   return phrase2
 }
-encrypter(show(text.value))
+function desencrypter() {
+  let text = document.querySelector(".text").value.toLowerCase()
+
+  let phrase2 = ""
+  let phrase = text
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ober", "o")
+    .replaceAll("ai", "a")
+    .replaceAll("ufat", "u")
+  if (phrase == "" || !phrase.includes("a", "e", "i", "o", "u")) {
+    alert("por favor ingrese una frase")
+  }
+  phrase2 = phrase
+  return phrase2
+}
+btnWhite.onclick = () => {
+  desencrypter()
+}
+btn.addEventListener("click", function () {
+  textareaView.focus()
+  document.execCommand("selectAll")
+  document.execCommand("copy")
+})
